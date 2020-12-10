803a47bff3ab5ef643561cb9bfb75e6533f5c5d5
DESKTOP-QUHF5E2\AMIT <amitsalunke01@gmail.com>
2020-12-10 19:18:06 +0530

    Added notification functionality in fb service

--- app/src/main/java/com/amitsalunke/firebasemsg1/MainActivity.kt
+++ app/src/main/java/com/amitsalunke/firebasemsg1/MainActivity.kt
@@ -8,13 +8,17 @@ import com.amitsalunke.firebasemsg1.databinding.ActivityMainBinding
 import com.amitsalunke.firebasemsg1.model.NotificationData
 import com.amitsalunke.firebasemsg1.model.PushNotification
 import com.amitsalunke.firebasemsg1.network.RetrofitInstance
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 
-const val TOPIC = "/topics/myTopic"
+const val TOPIC = "myTopic"
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -23,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+
         with(mBinding) {
             //val title = mBinding.
             btnSend.setOnClickListener {
@@ -35,8 +42,8 @@ class MainActivity : AppCompatActivity() {
                     ).also {
                         sendNotification(it)
                     }
-                }else{
-                    Log.e(TAG,"Please enter msg")
+                } else {
+                    Log.e(TAG, "Please enter title and msg")
                 }
             }
         }
@@ -50,7 +57,7 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     Log.e(TAG, "Is successful ${Gson().toJson(response)}")
                 } else {
-                    Log.e(TAG, "Error  ${response.errorBody().toString()}")
+                    Log.e(TAG, "Error  ${response.code()}")
                 }
             } catch (ex: Exception) {
                 Log.e(TAG, "Exception while sending the data : " + ex)
